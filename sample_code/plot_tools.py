1c8bd17d53076eaa444dba1e283c09acd9a42221
emilegill743 <58830015+emilegill743@users.noreply.github.com>
2020-11-08 07:44:30 +0000

    updating plot_tools.py to seperate annotations

--- sample_code/plot_tools.py
+++ sample_code/plot_tools.py
@@ -1,12 +1,6 @@
 from bokeh.plotting import figure, show
 
-p = figure(
-        plot_width=500,
-        plot_height=500,
-        x_axis_label="X Label",
-        y_axis_label="Y Label",
-        title="My custom plot",
-        toolbar_location="left",
-        tools="pan,wheel_zoom,reset")
+p = figure(toolbar_location="left",
+           tools="pan,wheel_zoom,reset")
         
 show(p)
